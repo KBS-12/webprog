488e2afa903a59461fbd8b1723dc5e4f0a50e30d
Tino Stojanoski <tino.stojanoski@co-it.eu>
2023-10-27 15:44:50 +0200

    Überarbeitet und seachpage

--- cookhub/src/app/app.module.ts
+++ cookhub/src/app/app.module.ts
@@ -9,7 +9,6 @@ import { LoginpageComponent } from './login-page/loginpage.component';
 import { SearchPageComponent } from './search-page/search-page.component';
 import { RecipePageComponent } from './recipe-page/recipe-page.component';
 import { RegisterPageComponent } from './register-page/register-page.component';
-import { NewUserPageComponent } from './new-user-page/new-user-page.component';
 import { NewRecipePageComponent } from './new-recipe-page/new-recipe-page.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from 'src/material.module';
@@ -28,7 +27,6 @@ import { AppRoutingModule } from './app-routing.module';
     SearchPageComponent,
     RecipePageComponent,
     RegisterPageComponent,
-    NewUserPageComponent,
     NewRecipePageComponent
   ],
   imports: [
